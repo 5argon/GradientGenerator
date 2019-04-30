470706ebf7f93356834f06f8d59be6ef82d362f4
5argon <pyasry@gmail.com>
2019-05-01 06:42:40 +0700

    first

---
+++ GradientGenerator.cs
@@ -0,0 +1,69 @@
+﻿// GradientGenerator.cs
+// 5argon / Sirawat Pitaksarit
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+//[CreateAssetMenu]
+public class GradientGenerator : ScriptableObject
+{
+    [Serializable]
+    public struct GradientSpecs
+    {
+        public AnimationCurve hProgression;
+        public AnimationCurve sProgression;
+        public AnimationCurve vProgression;
+        public AnimationCurve aProgression;
+        public AnimationCurve timeRemap;
+    }
+
+    public GradientSpecs[] gradientSpecs;
+    public Gradient[] generatedGradients;
+
+    [ContextMenu("Generate")]
+    public void Generate()
+    {
+        var gradients = new List<Gradient>();
+        for (int k = 0; k < gradientSpecs.Length; k++)
+        {
+            var spec = gradientSpecs[k];
+
+            var eightRainbow = Enumerable.Range(0, 9).Select(x => Mathf.InverseLerp(0, 9, x)).Take(8).Select(x =>
+            Color.HSVToRGB(spec.hProgression.Evaluate(x), spec.sProgression.Evaluate(x), spec.vProgression.Evaluate(x))).ToArray();
+
+            for (int mode = 0; mode < 2; mode++)
+            {
+                GradientMode gm = (GradientMode)mode;
+                var modePlus = gm == GradientMode.Fixed ? 1 : 0;
+                for (int i = 1; i < 8; i++)
+                {
+                    var timeRange = Enumerable.Range(0, i + 1).Select(x => Mathf.InverseLerp(0, i + modePlus, x + modePlus)).ToArray();
+                    var g = new Gradient();
+                    g.mode = gm;
+                    var colorKeys = new GradientColorKey[i + 1];
+                    var alphaKeys = new GradientAlphaKey[i + 1];
+                    for (int j = 0; j < i + 1; j++)
+                    {
+                        colorKeys[j].color = eightRainbow[j];
+                        colorKeys[j].time = spec.timeRemap.Evaluate(timeRange[j]);
+                        alphaKeys[j].alpha = spec.aProgression.Evaluate(timeRange[j]);
+                        alphaKeys[j].time = spec.timeRemap.Evaluate(timeRange[j]);
+                    }
+                    g.SetKeys(colorKeys, alphaKeys);
+                    gradients.Add(g);
+                }
+            }
+
+        }
+        generatedGradients = gradients.ToArray();
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(this);
+        AssetDatabase.SaveAssets();
+#endif
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Gradient Generator
+
+![how2](.howto/2.png)
+
+A Unity script to generate multiple variants of evenly distributed `Gradient` based on input `AnimationCurve` of HSV and alpha. Skew the distribution with `timeRemap` `AnimationCurve`.
+
+The generated gradients can be copied by the right click menu, then you could fine tune the color. You can keep the generated gradient in the generator since it is a `ScriptableObject` file.
+
+## How to use
+
+- Click on the `GradientGenerator` scriptable object file. If you lose it, uncomment `[CreateAssetMenu]`, then right click create a new one.
+- Provide `AnimationCurve` in the `gradientSpecs`. All curve values has to be in 0~1 range. To get a simple rainbow, use 0~1 "／" curve preset for Hue and `timeRemap`, then use a constant graph for S and V.
+- Right click the header and select `Generate` context menu.
+
+![how1](.howto/1.png)
+
+`generatedGradients` will be replaced with a new set. Each spec generates 7 `Blend` and 7 `Fixed` type of gradients.
+
+![how2](.howto/2.png)
+
+An example when using the `timeRemap` curve, the distribution could be skewed.
+
+![how3](.howto/3.png)
